1c2a1b103491d60a2541de5eedc4b0113e12478d
Gavin <gzhu100@icloud.com>
2022-10-08 11:25:17 -0700

    random color and angle square maker thing

--- src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
+++ src/_01_else_if/_1_robot_color_chooser/RobotColorChooser.java
@@ -3,6 +3,8 @@ package _01_else_if._1_robot_color_chooser;
 
 import javax.swing.JOptionPane;
 import java.awt.*;
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 
@@ -14,25 +16,53 @@ public class RobotColorChooser {
 		Robot rob = new Robot();
 		rob.penDown();
 	    //2. Make the robot draw a shape (this will take more than one line of code)
-		rob.move(160);
-		rob.setAngle(90);
-		rob.move(160);
-		rob.setAngle(180);
-		rob.move(160);
-		rob.setAngle(270);
-		rob.move(160);
+
 		//3. Set the pen width to 10
 		rob.setPenWidth(10);
+		rob.setSpeed(5);
 		//4. Ask the user what color pen they would like the robot to draw with
+		for (int i=0; i<100; i++){
+			//String penColor = JOptionPane.showInputDialog("What pen color would you like to draw with?");
+			//5. Use an if/else statement to set the pen color that the user requested
+			String penColor = "";
+			if (penColor.equalsIgnoreCase("blue")) {
+				rob.setPenColor(0, 0, 255);
+			}
+			else if (penColor.equalsIgnoreCase("red")){
+				rob.setPenColor(255,0,0);
+			}
+			else if (penColor.equalsIgnoreCase("orange")){
+				rob.setPenColor(255, 165, 0);
+			}
+			else if (penColor.equalsIgnoreCase("yellow")){
+				rob.setPenColor(Color.YELLOW);
+			}
+			else if (penColor.equalsIgnoreCase("green")){
+				rob.setPenColor(0,255,0);
+			}
+			else if (penColor.equalsIgnoreCase("purple")){
+				rob.setPenColor(161, 52, 235);
+			}
+			//6. If the user doesn't enter anything, choose a random color
+			else if (penColor.isEmpty()) {
+				rob.setRandomPenColor();
+			}
+			//7. Put a loop around your code so that you keep asking the user for more colors & drawing them
 
-		//5. Use an if/else statement to set the pen color that the user requested
-        //6. If the user doesn't enter anything, choose a random color
-        //7. Put a loop around your code so that you keep asking the user for more colors & drawing them
-		
 
-		
-
-
-
+			move(rob);
+		}
+	}
+	static void move(Robot rob){
+		Random idontcare = new Random();
+		int angle = idontcare.nextInt(360);
+		rob.setAngle(angle);
+		rob.move(160);
+		rob.setAngle(90+angle);
+		rob.move(160);
+		rob.setAngle(180+angle);
+		rob.move(160);
+		rob.setAngle(270+angle);
+		rob.move(160);
 	}
 }
